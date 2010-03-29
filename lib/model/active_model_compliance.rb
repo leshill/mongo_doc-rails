183bebe9a3ebcaafc5b494bfa8a670535afd39ce
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2010-03-29 21:29:01 +0100

    Initial commit of basic ActionPack compliance module for MongoDoc.

---
+++ lib/model/active_model_compliance.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+
+require 'active_model/naming'
+
+require 'active_model/deprecated_error_methods'
+require 'active_model/errors'
+
+module MongoDoc
+  module Model
+    
+    module ActiveModelCompliance
+      # Replace this with your own validations.
+      def valid?
+        true
+      end
+
+      def destroyed?
+        @_id.nil?
+      end
+      
+      def errors
+        @errors ||= ActiveModel::Errors.new(self)
+      end
+      
+      def self.included(doc_class)
+        doc_class.extend ActiveModel::Naming
+      end
+    end
+    
+  end
+end
