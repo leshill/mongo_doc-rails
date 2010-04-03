2587fa88bc3dcb8339d8cead1358d1976c2e7f80
Les Hill <leshill@gmail.com>
2010-04-03 13:04:00 -0400

    Cleanup

--- lib/model/active_model_compliance.rb
+++ lib/model/active_model_compliance.rb
@@ -1,13 +1,10 @@
-require 'rubygems'
-
 require 'active_model/naming'
-
 require 'active_model/deprecated_error_methods'
 require 'active_model/errors'
 
 module MongoDoc
   module Model
-    
+
     module ActiveModelCompliance
       # Replace this with your own validations.
       def valid?
@@ -17,15 +14,15 @@ module MongoDoc
       def destroyed?
         _id.nil?
       end
-      
+
       def errors
         @errors ||= ActiveModel::Errors.new(self)
       end
-      
+
       def self.included(doc_class)
         doc_class.extend ActiveModel::Naming
       end
     end
-    
+
   end
 end
